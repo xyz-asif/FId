e3d7f2fe20433360879e00a7383a449f8670cd15
xyz_asif <50095118+xyz-asif@users.noreply.github.com>
2023-05-26 08:35:03 +0530

    Update id.py
    updated

--- id.py
+++ id.py
@@ -1,7 +1,23 @@
 https://stackoverflow.com/questions/41912372/dlib-installation-on-windows-10
+https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+
+https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+
+pip install numpy
+
+pip install scipy
+
+pip install matplotlib
+
+pip install pandas
+
+python -m pip install  https://files.pythonhosted.org/packages/61/86/cc8d1ff2ca31a312a25a708c891cf9facbad4eae493b3872638db6785eb5/wheel-0.40.0-py3-none-any.whl
 
 
-
+python -m pip install
+https://files.pythonhosted.org/packages/0e/ce/f8a3cff33ac03a8219768f0694c5d703c8e037e6aba2e865f9bae22ed63c/dlib-19.8.1-cp36-cp36m-win_amd64.whl#sha256=794994fa2c54e7776659fddb148363a5556468a6d5d46be8dad311722d54bfcf
+    
+    
 import face_recognition
 import os
 import tkinter as tk
